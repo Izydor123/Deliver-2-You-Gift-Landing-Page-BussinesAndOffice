bec8ee407d18722dd2196755cd896bd11dc37262
Izydor123 <djqyubi@gmail.com>
2025-04-08 13:50:38 +0100

    e

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,16 @@ function App() {
       <Header title="Negócios e Escritório" desc1="Encontre as melhores soluções para equipar seu espaço de trabalho com eficiência e estilo" desc2="Produtos de escritório que impulsionam a produtividade e inspiram criatividade."/>
       <Product
        image={imageMain} 
-       title="Carpeta Multifunción Harbur: Tecnologia e Versatilidade num Só Objecto" 
+       title="Pasta Multifuncao Harbur: Tecnologia e Versatilidade num Só Objecto" 
        desc1="Esta pasta multifunção em poliéster 300D RPET representa o encontro perfeito entre funcionalidade moderna e design elegante. Com um acabamento em tom de cinzento sofisticado, destaca-se pela sua tecnologia integrada de MagSafe e carregamento Qi, permitindo carregar dispositivos sem fios de forma prática e eficiente."
        desc2="As suas dimensões compactas (23,5 cm x 2 cm x 31 cm) a tornam ideal para profissionais e estudantes que valorizam mobilidade e tecnologia. O material RPET demonstra ainda um compromisso com a sustentabilidade, sendo fabricado a partir de plástico reciclado."
        link="https://generalcatalogue2025.eu/deliver2yougift/#page/166"/>
       <ProductsList 
         data={[
-        {id:1, text:"Organizador Soporte Tronser", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/172", image:image1},
-        {id:2, text:"Proyector Heroux", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/175", image:image2},
-        {id:3, text:"Lapicero Multifunción Dowex", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/184", image:image3},
-        {id:4, text:"Tarjetero Restek", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/197", image:image4},
+        {id:1, text:"Organizador Suporte Tronser", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/172", image:image1},
+        {id:2, text:"Projetor Heroux", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/175", image:image2},
+        {id:3, text:"Porta-Lapis Multifuncao Dowex", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/184", image:image3},
+        {id:4, text:"Pora-Cartoes Restek", link:"https://generalcatalogue2025.eu/deliver2yougift/#page/197", image:image4},
         ]}
         title="Outros produtos desta categoria"/>
       <Catalogue text="Conheça os nossos outros produtos associados os negócios e escritório" link="https://generalcatalogue2025.eu/deliver2yougift/#page/164"/>
